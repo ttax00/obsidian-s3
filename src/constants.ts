3c4f68dc62f8215a74620e5676ac53bdf1d021e5
TechTheAwesome <techtheawesome01@gmail.com>
2022-11-11 01:13:54 +0900

    REMOVE un-linkable types

--- src/constants.ts
+++ src/constants.ts
@@ -2,19 +2,21 @@ export const ALLOWED_HEADERS =
 	'Access-Control-Allow-Headers, Origin, Authorization,Accept,x-client-id, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, hypothesis-client-version';
 
 export const mimeType = new Map([
-	['.ico', 'image/x-icon'],
 	['.html', 'text/html'],
-	['.js', 'text/javascript'],
-	['.json', 'application/json'],
+	// ['.js', 'text/javascript'],
+	['.ico', 'image/x-icon'],
 	['.css', 'text/css'],
 	['.png', 'image/png'],
 	['.jpg', 'image/jpeg'],
+	['.gif', 'image/gif'],
+	['.svg', 'image/svg+xml'],
 	['.wav', 'audio/wav'],
 	['.mp3', 'audio/mpeg'],
-	['.svg', 'image/svg+xml'],
-	['.pdf', 'application/pdf'],
-	['.zip', 'application/zip'],
-	['.doc', 'application/msword'],
-	['.eot', 'application/vnd.ms-fontobject'],
-	['.ttf', 'application/x-font-ttf']
+	['.mp4', 'video/mp4'],
+	// ['.json', 'application/json'],
+	// ['.pdf', 'application/pdf'],
+	// ['.zip', 'application/zip'],
+	// ['.doc', 'application/msword'],
+	// ['.eot', 'application/vnd.ms-fontobject'],
+	['.ttf', 'application/x-font-ttf'],
 ]);
