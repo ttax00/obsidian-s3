530e6a3d5813c64862792052ca990d08e081a83c
TechTheAwesome <techtheawesome01@gmail.com>
2022-11-10 18:18:49 +0900

    ADD functioning paste upload

--- main.ts
+++ main.ts
@@ -1,85 +1,71 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
+import { Client } from 'minio';
+import { SettingsTab } from 'src/settings';
+import exp from 'src/httpServer';
+import { mimeType } from 'src/constants';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	accessKey: string;
+	secretKey: string;
+	endPoint: string;
+	folderName: string;
+	port: string;
+	bucketName: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	accessKey: '',
+	secretKey: '',
+	endPoint: '',
+	folderName: 'obsidian',
+	port: '4998',
+	bucketName: '',
+}
+
+function allFilesAreValidUploads(files: FileList) {
+	if (files.length === 0) return false;
+
+	for (let i = 0; i < files.length; i += 1) {
+		if (Array.from(mimeType.values()).includes(files[i].type)) return false;
+	}
+
+	return true;
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	client: Client;
+	server: { listen(): void; close(): void; };
 
 	async onload() {
+		console.log("Loading Obsidian Storj");
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		console.log("obsidian storj loaded with settings:");
+		console.log(this.settings);
+		const { endPoint, accessKey, secretKey, port, bucketName, folderName } = this.settings;
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingsTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Create s3 client.
+		this.client = new Client({
+			endPoint,
+			useSSL: true,
+			accessKey,
+			secretKey
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Spawn http server 
+		this.server = exp(this.client, bucketName, folderName, port);
+		this.server.listen();
+
+		this.setupHandlers();
 	}
 
 	onunload() {
-
+		this.server.close();
 	}
 
 	async loadSettings() {
@@ -89,49 +75,60 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	private async uploadFile(file: File) {
+		const { bucketName, folderName } = this.settings;
+		console.log(file);
+		const buf = Buffer.from(await file.arrayBuffer());
+		const name = folderName + '/' + file.name;
+		console.log(buf);
+		console.log(name);
+
+
+		this.client.putObject(bucketName, name, buf, (e, r) => {
+			if (e) {
+				console.log(e);
+			} else {
+				console.log(r);
+			}
+		});
+	}
+
+	private pasteEventHandler = async (e: ClipboardEvent, _: Editor, markdownView: MarkdownView) => {
+		if (!this.client) {
+			return;
+		}
+		if (!e.clipboardData) return;
+		if (e.clipboardData.files.length === 0) return;
+		const files = e.clipboardData.files;
+
+		console.log(files);
+		if (!allFilesAreValidUploads(files)) return;
+		e.preventDefault();
+		console.log('boo');
+
+		for (let i = 0; i < files.length; i += 1) {
+			try {
+				await this.uploadFile(files[i]);
+			}
+			catch (e) {
+				console.log(e);
+				return;
+			}
+		}
+	}
+	private async dropEventHandler() {
+
+	}
+
+	private setupHandlers() {
+		this.registerEvent(
+			this.app.workspace.on("editor-paste", this.pasteEventHandler)
+		);
+		this.registerEvent(
+			this.app.workspace.on("editor-drop", this.dropEventHandler.bind(this))
+		);
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
