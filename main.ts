cf69d096a6b01128da8eddb395c5689ca933d331
TechTheAwesome <techtheawesome01@gmail.com>
2022-11-11 14:13:59 +0900

    ADD drop event handler

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import toIt from 'blob-to-it'
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface ObsidianS3Settings {
 	accessKey: string;
 	secretKey: string;
 	endPoint: string;
@@ -17,7 +17,7 @@ interface MyPluginSettings {
 	bucketName: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: ObsidianS3Settings = {
 	accessKey: '',
 	secretKey: '',
 	endPoint: '',
@@ -39,13 +39,13 @@ function allFilesAreValidUploads(files: FileList) {
 	return true;
 }
 
-function isValidSettings(settings: MyPluginSettings) {
+function isValidSettings(settings: ObsidianS3Settings) {
 	if (settings.accessKey != '' && settings.secretKey != '' && settings.endPoint != '' && settings.bucketName != '') return true;
 	return false;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ObsidianS3 extends Plugin {
+	settings: ObsidianS3Settings;
 	pluginName = "Obsidian S3";
 	client: Client;
 	server: { listen(): void; close(): void; };
@@ -60,6 +60,7 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingsTab(this.app, this));
+		this.setupHandlers();
 
 		this.tryStartService();
 	}
@@ -68,6 +69,7 @@ export default class MyPlugin extends Plugin {
 		const { endPoint, accessKey, secretKey, port, bucketName } = this.settings;
 		// Only create clients when settings are valid.
 		if (isValidSettings(this.settings)) {
+			new Notice(`Creating S3 Client`);
 			this.client = new Client({
 				endPoint,
 				useSSL: true,
@@ -79,7 +81,6 @@ export default class MyPlugin extends Plugin {
 			this.server = exp(this.client, bucketName, port);
 			this.server.listen();
 
-			this.setupHandlers();
 		} else {
 			new Notice("Please fill out Obsidian S3 settings tab to enable the plugin.");
 		}
@@ -117,8 +118,8 @@ export default class MyPlugin extends Plugin {
 				this.registerInterval(handle);
 				readable.on('close', () => {
 					window.clearInterval(handle);
-					new Notice(`Uploading: ${name} ${Math.round(progress / file.size * 100)}%`)
-					new Notice('Creating link...')
+					new Notice(`Upload complete.`);
+					new Notice('Creating link...');
 				})
 				const result = await this.client.putObject(bucketName, name, readable, file.size);
 
@@ -126,7 +127,7 @@ export default class MyPlugin extends Plugin {
 				console.log(result);
 			}
 			catch (e) {
-				new Notice(`Error: Unable to upload ${fileName}, see console for more details.`);
+				new Notice(`Error: Unable to upload ${fileName}. See details in console.`);
 				console.log(e);
 				return;
 			}
@@ -154,17 +155,20 @@ export default class MyPlugin extends Plugin {
 
 		const cursor = editor.getCursor()
 		const line = editor.getLine(cursor.line)
+
 		// console.log('editor context', cursor, )
 		editor.transaction({
 			changes: [
 				{
-					from: { ...cursor, ch: 0 },
-					to: { ...cursor, ch: line.length },
-					text: newLinkText,
+
+					from: { ...cursor, ch: 0, },
+					to: { ...cursor, ch: line.length, },
+					text: newLinkText + "\n",
 				}
 			]
 		})
-
+		cursor.line += 1;
+		editor.setCursor(cursor);
 		new Notice("Link created.");
 	}
 
@@ -189,12 +193,29 @@ export default class MyPlugin extends Plugin {
 		console.log(files);
 		if (!allFilesAreValidUploads(files)) return;
 		e.preventDefault();
-		console.log('boo');
 
 		await this.uploadFiles(files);
 	}
-	private async dropEventHandler() {
+	private async dropEventHandler(e: DragEvent, _: Editor, markdownView: MarkdownView) {
+		if (!this.client) {
+			new Notice("Please fill out Obsidian S3 settings tab to enable the plugin.")
+			return;
+		}
+		if (!e.dataTransfer) return;
 
+		console.log(e);
+
+		if (
+			e.dataTransfer.types.length !== 1 ||
+			!e.dataTransfer.types.includes("Files")
+		) {
+			return;
+		}
+
+		const { files } = e.dataTransfer;
+
+		e.preventDefault();
+		await this.uploadFiles(files);
 	}
 
 	private setupHandlers() {
